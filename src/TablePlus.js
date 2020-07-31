cea72880e430edbbd0128d237795829035554aa8
Aubrey Portwood <code@aubreypwd.com>
2020-07-31 14:52:27 -0600

    Working plugin.

--- src/TablePlus.js
+++ src/TablePlus.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { TextButton } from '@getflywheel/local-components';
 import is from 'electron-is';
 
+const { exec } = require( 'child_process' );
 const fs = require( 'fs' );
 
 export default class TablePlus extends React.Component {
@@ -15,7 +16,34 @@ export default class TablePlus extends React.Component {
 	componentWillUnmount() {
 	}
 
+	getSockFile() {
+		return `${this.props.site.paths.runData}/mysql/mysqld.sock`;
+	}
+
+	linkTmpMysqlSock( callback ) {
+		fs.access( '/tmp/mysql.sock', fs.constants.F_OK, ( err ) => {
+			if ( err ) {
+				console.log( err );
+			}
+
+			fs.unlink( '/tmp/mysql.sock', ( err ) => {
+				fs.symlinkSync( this.getSockFile(), '/tmp/mysql.sock', 'file', ( err ) => {
+					console.log( err );
+				} );
+			} );
+
+			callback();
+		} );
+	}
+
 	connect() {
+		this.linkTmpMysqlSock( () => {
+			let uri = `mysql://${this.props.site.mysql.user}:${this.props.site.mysql.password}@localhost/${this.props.site.mysql.database}`;
+
+			exec( `open "${uri}"`, ( err, stdout, stderr ) => {
+				return;
+			} );
+		} );
 	}
 
 	getButtonLabel () {
